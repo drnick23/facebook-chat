83ec2f45dc5a951e7be5df0c7bec9070b126099c
Nicolas Halper <nick@nicolass-mbp.d.baby-gaga.com>
2014-06-30 09:56:46 -0700

    Added comments and minor code cleanups

--- facebook-chat/NewsFeedViewController.m
+++ facebook-chat/NewsFeedViewController.m
@@ -9,12 +9,16 @@
 #import "NewsFeedViewController.h"
 
 @interface NewsFeedViewController ()
-@property (weak, nonatomic) IBOutlet UIScrollView *newsFeedScrollView;
-@property (weak, nonatomic) IBOutlet UIView *newsFeedView;
+
 - (IBAction)onDarren:(UIButton *)sender;
-@property (weak, nonatomic) IBOutlet UIImageView *chatHead;
 - (IBAction)onChatHeadPan:(UIPanGestureRecognizer *)sender;
+
 @property (weak, nonatomic) IBOutlet UIImageView *trashImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *chatHead;
+
+@property (weak, nonatomic) IBOutlet UIView *newsFeedView;
+@property (weak, nonatomic) IBOutlet UIScrollView *newsFeedScrollView;
+
 
 @end
 
@@ -32,31 +36,31 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
     
-    self.newsFeedView.center = CGPointMake(160, self.newsFeedView.center.y);
-    self.title = @"News Feed";
+    // hide the chat head on startup
     self.chatHead.hidden = YES;
-    self.chatHead.center = CGPointMake(400, self.chatHead.center.y);
+    
+    // newsFeed setup with position and size of scroll view
+    self.newsFeedView.center = CGPointMake(160, self.newsFeedView.center.y);
     self.newsFeedScrollView.contentSize = CGSizeMake(320, 1300);
     
+    // place the trash image view offscreen, ready to move in when needed
     self.trashImageView.center = CGPointMake(160, 750);
 
-//    UIImage *rightButtonImage = [[UIImage imageNamed:@"friends"]]
-    
-    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Friends" style:UIBarButtonItemStylePlain target:self action:@selector(onRightButton)];
+    // configure the navigation bar with a title and a right navigation bar button "Friends"
+    self.title = @"News Feed";
+    UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:@"Friends" style:UIBarButtonItemStylePlain target:self action:@selector(onFriendsNavigationButton)];
     self.navigationItem.rightBarButtonItem = rightButton;
     
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (void) onRightButton {
-    NSLog(@"Right button pressed");
+- (void) onFriendsNavigationButton {
+    NSLog(@"onFriendsNavigationButton");
+    
+    // when we press the friends navigation button,
+    //    if the newsfeed is all the way to the right (closed), slide it open to the left to reveal the friends list view underneath it
+    //    otherwise, slide the newsfeed back "closed" over the friends list.
+    
     if(self.newsFeedView.center.x == 160){
         [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
             self.newsFeedView.center = CGPointMake(-40, self.newsFeedView.center.y);
@@ -69,32 +73,57 @@
 }
 
 - (IBAction)onDarren:(UIButton *)sender {
-    NSLog(@"I work!");
+    NSLog(@"onDarren");
+    
+    // when a friend is selected from our friends list,
+    //   - position the chat head off screen at the point we want to slide it in
+    //   - "close" the friends list menu by sliding the newsfeed back across
+    //   - then animate the chat head into the screen
+    
+    // hide the chat head and position off screen ready to move in
+    self.chatHead.hidden = YES;
+    self.chatHead.center = CGPointMake(400, 120);
+    
     [UIView animateWithDuration:.2 delay:0 options:UIViewAnimationOptionBeginFromCurrentState animations:^{
+        // slide the newsfeed "closed" to hide the friends menu
         self.newsFeedView.center = CGPointMake(160, self.newsFeedView.center.y);
     } completion:^(BOOL finished) {
+        // once the newsfeed has finished sliding across, unhide the chat head
         self.chatHead.hidden = NO;
         [UIView animateWithDuration:.3 animations:^{
+            // and then slide the chathead to the top right of the screen
             self.chatHead.center = CGPointMake(250, 120);
         }];
     }];
 }
 - (IBAction)onChatHeadPan:(UIPanGestureRecognizer *)sender {
-    NSLog(@"CHat head panning");
+    NSLog(@"onChatHeadPan");
+    
+    // when we pan on a chat head
+    
     if (sender.state == UIGestureRecognizerStateBegan) {
         
+        // - at the start of the gesture:
+        //    - slide in the trashImageView from the bottom into the screen
+        
         [UIView animateWithDuration:.3 animations:^{
-            self.trashImageView.center = CGPointMake(160, 400);
+            self.trashImageView.center = CGPointMake(160, 480);
         }];
         
-    } else if (sender.state == UIGestureRecognizerStateChanged  ){
-        self.chatHead.center = [sender locationInView:self.view];
+    } else if (sender.state == UIGestureRecognizerStateChanged) {
         
+        // - during the gesture:
+        //    - update the chat head position to the current touch point of the gesture
+        
+        self.chatHead.center = [sender locationInView:self.view];
         
     } else if (sender.state == UIGestureRecognizerStateEnded){
         
+        // - after the gesture:
+        //    - if the chat head is below the trashImageView, slide it off the button of the screen to remove it
+        //    - otherwise snap the chat head to the left or right bounds of the screen
+        
         [UIView animateWithDuration:.3 delay:0 usingSpringWithDamping:.5 initialSpringVelocity:40 options:0 animations:^{
-            
             if (self.chatHead.center.y > self.trashImageView.frame.origin.y) {
                 self.chatHead.center = CGPointMake(160, 600);
             } else if (self.chatHead.center.x < 160) {
@@ -104,12 +133,10 @@
             }
         } completion:nil];
         
+        //    - slide the trashImageView away
         [UIView animateWithDuration:.3 animations:^{
             self.trashImageView.center = CGPointMake(160, 750);
         }];
-        
-        
-        
     }
 }
 @end
